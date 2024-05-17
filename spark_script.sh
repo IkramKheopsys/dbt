163c5bcd31617fb22d36d430f1636f86870ddbdc
IkramKheopsys <113558455+IkramKheopsys@users.noreply.github.com>
2024-05-17 16:42:49 +0200

    Update spark_script.sh

--- spark_script.sh
+++ spark_script.sh
@@ -1,43 +1,16 @@
-##!/bin/bash
-#sudo apt-get update
-#apt-get install -y apt-transport-https ca-certificates curl gnupg software-properties-common
-## Ajouter la clé GPG de Docker
-#sudo curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-## Ajouter le repository de Docker
-#sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get update
-#sudo apt-get install -y docker.io
-#sudo systemctl start docker
-#sudo systemctl enable docker
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
-#
-## Créer un répertoire pour le cluster Spark
-#mkdir -p /spark_cluster
-#cd /spark_cluster
-#
-
-
-#start-spark.sh
 #!/bin/bash
-. "/opt/spark/bin/load-spark-env.sh"
-# When the spark work_load is master run class org.apache.spark.deploy.master.Master
+
+SPARK_WORKLOAD=$1
+
+echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
+
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
-
-export SPARK_MASTER_HOST=`hostname`
-
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
-
+  start-master.sh -p 7077
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
-# When the spark work_load is worker run class org.apache.spark.deploy.master.Worker
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
-
-elif [ "$SPARK_WORKLOAD" == "submit" ];
+  start-worker.sh spark://spark-master:7077
+elif [ "$SPARK_WORKLOAD" == "history" ]
 then
-    echo "SPARK SUBMIT"
-else
-    echo "Undefined Workload Type $SPARK_WORKLOAD, must specify: master, worker, submit"
+  start-history-server.sh
 fi
-
